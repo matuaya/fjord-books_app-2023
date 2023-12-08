aefe99153cf9f5635d0f9d4653800d9f256107ef
matsuyama <aya.k.matsuyama@gmail.com>
2023-12-09 04:21:05 +0900

    migrationファイルをテーブル毎に統合し、不要なものは削除した

--- db/migrate/20231129111220_create_reports.rb
+++ db/migrate/20231129111220_create_reports.rb
@@ -4,6 +4,7 @@ class CreateReports < ActiveRecord::Migration[7.0]
       t.string :title
       t.date :date
       t.text :content
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
