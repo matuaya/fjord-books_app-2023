7b00a5c4357eb87eab840081d3809345174aebbc
matsuyama <aya.k.matsuyama@gmail.com>
2023-12-21 23:25:38 +0900

    relationshipsテーブルに外部キーとnull制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -60,11 +60,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_05_193240) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "mentioning_id"
-    t.integer "mentioned_id"
+    t.integer "mentioning_id", null: false
+    t.integer "mentioned_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["mentioned_id"], name: "index_relationships_on_mentioned_id"
     t.index ["mentioning_id", "mentioned_id"], name: "index_relationships_on_mentioning_id_and_mentioned_id", unique: true
+    t.index ["mentioning_id"], name: "index_relationships_on_mentioning_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -95,5 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_05_193240) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "relationships", "reports", column: "mentioned_id"
+  add_foreign_key "relationships", "reports", column: "mentioning_id"
   add_foreign_key "reports", "users"
 end
