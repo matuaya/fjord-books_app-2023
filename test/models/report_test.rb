0ed744b3933fb2d7f95bfaa5256eb275ea08c89c
matsuyama <aya.k.matsuyama@gmail.com>
2024-01-02 05:24:27 +0900

    テストコードの作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,57 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable? should return true when current user and false when otherwise' do
+    current_user = create(:user)
+    another_user = create(:user, name: 'bob')
+    current_user_report = build(:report, user: current_user)
+
+    assert current_user_report.editable?(current_user)
+    assert_not current_user_report.editable?(another_user)
+  end
+
+  test 'create_on should return formatted date' do
+    report = create(:report)
+
+    assert_equal(Time.zone.today, report.created_on)
+  end
+
+  test 'mentions should not be created under certain conditions' do
+    bob_report = create(:report)
+    charlie_report = create(:report)
+    alice_report = create(:report, content: "http://localhost:3000/reports/#{bob_report.id}の日報と、" \
+                                            "http://localhost:3000/reports/#{bob_report.id}の日報と、" \
+                                            "http://localhost:3000/reports/#{charlie_report.id}の日報と、" \
+                                            "http://localhost/reports/#{charlie_report.id}の日報と、" \
+                                            'http://localhost:3000/reports/1の日報')
+
+    alice_report_mentioning_ids = alice_report.mentioning_reports.map(&:id).sort
+
+    # 重複しているidは一意にし、レポートが存在しない場合やマッチしないURLの場合はメンションを作らない
+    assert_equal([bob_report.id, charlie_report.id].sort, alice_report_mentioning_ids)
+  end
+
+  test 'mentions should be removed if associated reports deleted during update' do
+    bob_report = create(:report)
+    charlie_report = create(:report)
+    dave_report = create(:report)
+    alice_report = create(:report, content: "http://localhost:3000/reports/#{bob_report.id}の日報と、" \
+                                            "http://localhost:3000/reports/#{charlie_report.id}の日報")
+
+    alice_report.update!(content: "http://localhost:3000/reports/#{charlie_report.id}の日報と、"\
+                                  "http://localhost:3000/reports/#{dave_report.id}の日報")
+    alice_report.mentioning_reports.reload
+
+    alice_reports_mentioning_ids = alice_report.mentioning_reports.map(&:id)
+
+    # 削除されたレポートのメンションは削除され、新たに追加されたレポートは新たにメンションを作成する
+    assert_equal([charlie_report.id, dave_report.id], alice_reports_mentioning_ids)
+  end
+
+  test 'metions should be removed when report is deleted' do
+    bob_report = create(:report)
+    alice_report = create(:report, content: "http://localhost:3000/reports/#{bob_report.id}の日報")
+    alice_report.destroy!
+    assert_equal([], bob_report.mentioned_reports)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name_or_email should return email when name is nil' do
+    alice = build(:user, name: '', email: 'alice@example.com', password: 'password')
+    bob = build(:user, name: 'bob', email: 'bob@example.com', password: 'password')
+
+    assert_equal('alice@example.com', alice.name_or_email)
+    assert_equal('bob', bob.name_or_email)
+  end
 end
